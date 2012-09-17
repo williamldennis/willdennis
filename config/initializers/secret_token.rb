1aac273a98e4f0f8b972d1222ad7ef164908cf34
Will Dennis <williamldennis@gmail.com>
2012-09-16 23:22:50 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Willdennis::Application.config.secret_token = 'd33345a2c246b17e88cbc35623b2b4f2b23edf59f77a6849dac82d8308096249b082fe35d9db3189dea7323fa01c2254fe5b052d573780857ca945c865355669'
